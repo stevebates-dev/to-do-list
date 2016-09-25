6a99aa9c90abe8c825a8ec562f2ace81d2c67995
Steve Bates <stevebatesjr@gmail.com>
2016-09-25 09:39:40 -0700

    Add files via upload

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,63 +1,35 @@
-var listItemSetup = (function (){
 
-  return {
-    init : function () {
-      //hide last new danger button
-      $(".btn-danger").last().hide();
+function deleteButtonSetup () {
+  //remove the current li if trash button pressed, 
+  $("ul").on("click", "span", function (e) {
+    $(this).parent().fadeOut( 200, function () {
+      $(this).remove();
+    });
+    e.stopPropagation();
+  });
+}
 
-      //set parent li to height of child trash button
-      $("li").last().css("height", $(".btn-danger").outerHeight());
+deleteButtonSetup ();
 
-      //add strikethrough on li click
-      $("li").last().click(function () {
-        $(this).toggleClass("strike");
-      });
 
-      //show trash button on li hover, hide on hover remove
-      $("li").last().hover(function () {
-        $(this).children("a").show();
-        $(this).css("padding", "0px");
-      }, 
-      function () {
-        $(this).children("a").hide();
-        $(this).css("padding", "5px");
-      });
+$("#todo-list").on("click", "li", function () {
+  $(this).toggleClass("strike");
+});
 
-      //remove the current li if trash button pressed, 
-      $(".btn-danger").last().on("click", function (e) {
-        $(this).parent().remove();
-        e.stopPropagation();
-      });
+$(".fa-plus").click( function () {
+  //hide/show new to-do input
+  $("#new-todo").fadeToggle();
+});
 
+$("#new-todo").keypress(function(e){
+    //if enter is pressed in text input
+    if(e.keyCode === 13)
+    {
+        var trashButtonStr = '<span><i class="fa fa-trash-o fa-lg"></i></span>';
+        var str = $(this).val();
+        $("#todo-list").append('<li>' + trashButtonStr + 
+        str + '</li>');
+        $(this).val('');
+        deleteButtonSetup ();
     }
-  };
-})();
-
-var plusButtonHandler = (function () {
-
-  var trashButtonStr = '<a class="btn btn-danger" display="none" href="#"><i class="fa fa-trash-o fa-lg"></i></a>';
-
-  return {
-    init : function  () {
-
-      $("#plus-button").click( function () {
-        //hide/show new to-do input
-        $("#new-todo").toggle();
-      });
-
-      $("#new-todo").keypress(function(e){
-          //if enter is pressed in text input
-          if(e.keyCode == 13)
-          {
-              $("#todo-list").append('<li class="list-group-item">' + trashButtonStr + 
-                '<span id="to-do-span">' + $(this).val() + '</span></li>');
-              $(this).val('');
-              listItemSetup.init();
-          }
-      });
-    }
-  };
-})();
-
-
-plusButtonHandler.init();
+});
